347596504b212430706a18ddabff749a1d0b4ec3
Jakub Waller <j.waller@qimia.de>
2020-08-14 16:09:54 +0200

    java->scala convertor is not perfect

--- uhrwerk-common/src/main/scala/io/qimia/uhrwerk/common/framemanager/BulkDependencyResult.scala
+++ uhrwerk-common/src/main/scala/io/qimia/uhrwerk/common/framemanager/BulkDependencyResult.scala
@@ -2,11 +2,11 @@ package io.qimia.uhrwerk.common.framemanager
 
 import java.time.LocalDateTime
 
-import io.qimia.uhrwerk.common.model.{Connection, Dependency}
+import io.qimia.uhrwerk.common.model.{Connection, Dependency, Partition}
 
 case class BulkDependencyResult(
                                  partitionTimestamps: Array[LocalDateTime],
                                  dependency: Dependency,
                                  connection: Connection,
-                                 succeeded: Boolean
+                                 succeeded: Array[Partition]
                                )
