42d7101e3885cfddbb09b643d5d3898da6ba0b7a
Saba Fallah <s.fallah@qimia.de>
2020-08-18 18:21:21 +0200

    Dependency setKey corrected

--- uhrwerk-common/src/main/java/io/qimia/uhrwerk/common/model/Dependency.java
+++ uhrwerk-common/src/main/java/io/qimia/uhrwerk/common/model/Dependency.java
@@ -27,7 +27,7 @@ public class Dependency {
             .append(this.getVersion());
     long depTableId = LongHashFunction.xx().hashChars(res);
     StringBuilder res2 = new StringBuilder().append(depTableId).append(this.getFormat());
-    long targetId = LongHashFunction.xx().hashChars(res);
+    long targetId = LongHashFunction.xx().hashChars(res2);
     setTargetId(targetId);
     StringBuilder res3 = new StringBuilder().append(this.getTableId()).append(depTableId).append(this.targetId);
     long id = LongHashFunction.xx().hashChars(res3);
