ba8848310bea8feb68acaf16b5f1d0c0c6e3c6fe
Jakub Waller <j.waller@qimia.de>
2020-09-21 09:33:26 +0200

    adjusted examples

--- uhrwerk-example/src/main/scala/io/qimia/uhrwerk/example/yelp/CombinerC.scala
+++ uhrwerk-example/src/main/scala/io/qimia/uhrwerk/example/yelp/CombinerC.scala
@@ -2,8 +2,8 @@ package io.qimia.uhrwerk.example.yelp
 
 import java.time.LocalDateTime
 
-import io.qimia.uhrwerk.engine.{Environment, TaskInput, TaskOutput}
 import io.qimia.uhrwerk.engine.Environment.TableIdent
+import io.qimia.uhrwerk.engine.{Environment, TaskInput, TaskOutput}
 import io.qimia.uhrwerk.framemanager.SparkFrameManager
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.monotonically_increasing_id
@@ -18,9 +18,9 @@ object CombinerC extends App {
 
   def CombinerCFunc(in: TaskInput): TaskOutput = {
     // The most basic userFunction simply returns the input dataframe
-    val aDF = in.loadedInputFrames(TableIdent("staging", "yelp_db", "table_a_parq", "1.0"))
+    val aDF = in.loadedInputFrames(TableIdent("staging", "yelp_db", "table_a", "1.0"))
       .drop("id", "user_id", "year", "month", "day", "hour", "minute", "date")
-    val bDF = in.loadedInputFrames(TableIdent("staging", "yelp_db", "table_b_parq", "1.0"))
+    val bDF = in.loadedInputFrames(TableIdent("staging", "yelp_db", "table_b", "1.0"))
       .drop("id", "business_id")
       .withColumnRenamed("text", "othertext")
     val outDF = aDF
@@ -39,11 +39,7 @@ object CombinerC extends App {
     uhrwerkEnvironment.addTableFile("combiner-C-parq.yml", CombinerCFunc, true)
 
   val runTimes = Array(
-    LocalDateTime.of(2012, 5, 1, 0, 0),
-    LocalDateTime.of(2012, 5, 2, 0, 0),
-    LocalDateTime.of(2012, 5, 3, 0, 0),
-    LocalDateTime.of(2012, 5, 4, 0, 0),
-    LocalDateTime.of(2012, 5, 5, 0, 0)
+    LocalDateTime.of(2012, 5, 2, 3, 4)
   )
   val results = wrapper.get.runTasksAndWait(runTimes, false)
   println(results)
