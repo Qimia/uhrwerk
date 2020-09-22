a711d3a6eb01597bf3220be98e08ea213823ada3
Falk Schykowski <f.schykowski@qimia.de>
2020-09-22 14:16:39 +0200

    Merge branch 'retail_examples' of vmd36290.contaboserver.net:uhrwerk/uhrwerk into retail_examples fixed transformation function First draft using multiple components

--- uhrwerk-cli/src/main/scala/io/qimia/uhrwerk/cli/CommandLineInterface.scala
+++ uhrwerk-cli/src/main/scala/io/qimia/uhrwerk/cli/CommandLineInterface.scala
@@ -1,55 +1,82 @@
 package io.qimia.uhrwerk.cli
 
+import java.time.LocalDateTime
 import java.util
 import java.util.concurrent.Callable
 
+import io.qimia.uhrwerk.engine.Environment.TableIdent
 import picocli.CommandLine
 import picocli.CommandLine.{Command, Option}
+import io.qimia.uhrwerk.engine.UhrwerkAppRunner
+import org.apache.spark.sql.SparkSession
 
-@Command(name="Uhrwerk", version=Array("Scala picocli 1.0"),
+import scala.collection.JavaConverters._
+
+@Command(name = "Uhrwerk", version = Array("Scala picocli 1.0"),
   mixinStandardHelpOptions = true, description = Array("Run Uhrwerk from Command Line"))
-class CommandLineInterface extends Callable[Int]{
+class CommandLineInterface extends Callable[Int] {
 
-  @Option(names = Array("-g","--global"), paramLabel = "ENV_CONF",
-    description = Array("Path to the Environment Configuration"))
+  @Option(names = Array("-g", "--global"), paramLabel = "ENV_CONF",
+    description = Array("Path to the Environment Configuration"), required = false)
   private val environmentConfig = ""
 
-  @Option(names = Array("-c", "--cons"), paramLabel="CON_CONF",
-    description = Array("Paths to the Connection Configuration(s)"))
-  private val connectionConf = new util.ArrayList[String]
+  @Option(names = Array("-c", "--cons"), paramLabel = "CON_CONF",
+    description = Array("Paths to the Connection Configuration(s)"), required = false)
+  private val connectionConfAL = new util.ArrayList[String]
 
-  @Option(names = Array("-t", "--tables"), paramLabel="TAB_CONF",
-    description = Array("Paths to the Table Configuration(s)"))
-  private val tableConf = new util.ArrayList[String]
+  @Option(names = Array("-t", "--tables"), paramLabel = "TAB_CONF",
+    description = Array("Paths to the Table Configuration(s)"), required = false)
+  private val tableConfAL = new util.ArrayList[String]
 
   @Option(names = Array("-r", "--run"), paramLabel = "RUN_TAB",
-    description = Array("Table to run as 'area.vertical.table.version'")
-  )
+    description = Array("Table to run as 'area.vertical.table.version'"), required = true)
   private val runTable = ""
 
-  @Option(names = Array("-st", "--start"), paramLabel="STARTTS",
-    description = Array("Start point for the execution"))
+  @Option(names = Array("-st", "--start"), paramLabel = "STARTTS",
+    description = Array("Start point for the execution"), required = true)
   private val startTime = ""
 
   @Option(names = Array("-et", "--end"), paramLabel = "ENDTS",
-    description = Array("End point for the execution"))
+    description = Array("End point for the execution"), required = true)
   private val endTime = ""
 
   @Option(names = Array("-d", "--dag"), paramLabel = "DAGMD",
-    description = Array(""))
+    description = Array(""), required = true)
   private val dagM = "n"
 
   @Option(names = Array("-p", "--parallel"), paramLabel = "PARALLEL",
-    description = Array(""))
+    description = Array("Size of the threadpool for running the tasks"), required = false)
   private val parallelRun = 1
 
   @Option(names = Array("-o", "--ovw"), paramLabel = "OVERWR",
-    description = Array(""))
-  private val overwrite = "n"
+    description = Array("If entries should be updated if they already exist"), required = false)
+  private val overw = "n"
 
   override def call(): Int = {
+    val dagMode = dagM match {
+      case "y" => true
+      case _ => false
+    }
+    val overwrite = overw match {
+      case "y" => true
+      case _ => false
+    }
+    val sparkSess = SparkSession.builder().appName(runTable).master("local").getOrCreate()
 
-    0
+    val connectionConf = connectionConfAL.asScala.toArray
+    val tableConf = tableConfAL.asScala.toArray
+
+    val components = runTable.split(".")
+    val target = TableIdent(components(0), components(1), components(2), components(3))
+
+    try {
+      UhrwerkAppRunner.runFiles(sparkSess, environmentConfig, connectionConf,
+        tableConf, target, startTime.asInstanceOf[LocalDateTime], endTime.asInstanceOf[LocalDateTime], dagMode, parallelRun, overwrite)
+      0
+    }
+    catch {
+      case e: Exception => 1
+    }
   }
 }
 
@@ -58,3 +85,4 @@ object CommandLineInterface{
     System.exit(new CommandLine(new CommandLineInterface()).execute(args: _*))
   }
 }
+
