6013522b493cbb5493663f4b17e339d134bff9ca
Falk Schykowski <f.schykowski@qimia.de>
2020-09-24 15:23:12 +0200

    submit script placeholders changed to fat jar and absolute paths change target to dot seperated change target to dot seperated

--- uhrwerk-cli/src/test/resources/submit.sh
+++ uhrwerk-cli/src/test/resources/submit.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 /home/falk/Documents/spark-2.4.4-bin-hadoop2.7/bin/spark-submit \
   --class io.qimia.uhrwerk.cli.CommandLineInterface \
-  --jars /home/falk/IdeaProjects/uhrwerk/uhrwerk-example/target/uhrwerk-example-0.1.0-SNAPSHOT-jar-with-dependencies.jar \
+  --jars /home/falk/IdeaProjects/uhrwerk/uhrwerk-example/target/uhrwerk-example-0.1.0-SNAPSHOT.jar \
   --master "local[*]" \
   /home/falk/IdeaProjects/uhrwerk/uhrwerk-cli/target/uhrwerk-cli-0.1.0-SNAPSHOT-jar-with-dependencies.jar \
   -g "/home/falk/IdeaProjects/uhrwerk/uhrwerk-example/src/main/resources/testing-env-config.yml" \
   -c "/home/falk/IdeaProjects/uhrwerk/uhrwerk-example/src/main/resources/testing-connection-config.yml" \
   -t "/home/falk/IdeaProjects/uhrwerk/uhrwerk-example/src/main/resources/retail_examples/staging.retail/productDimApp_1.0.yml" \
-  -r "staging_retail_productDimApp_1.0" \
+  -r "staging.retail.productDimApp.1.0" \
   -st "2020-06-01T00:00:00" \
   -et "2020-06-03T00:00:00"
