5e8e20dd9719562081f6fda84ebe36633b126421
Frans van den Heuvel <f.vandenheuvel@qimia.de>
2020-09-25 17:59:10 +0200

    Checking partitions by cli can be adjusted by table and time (range)

--- cli-tools/templates/query_partitions.sql
+++ cli-tools/templates/query_partitions.sql
@@ -1,18 +1,18 @@
 SELECT
+	tabtar.table_name,
 	tabtar.target_format,
 	par.partition_ts,
 	par.partitioned
 FROM (SELECT 
 		tab.id as table_id,
+		tab.name as table_name,
 		tar.id as target_id,
 		tar.format as target_format
 	FROM TABLE_ tab
 	JOIN TARGET tar
 	ON tab.id = tar.table_id
-	WHERE tab.area = %s
-	AND tab.vertical = %s
-	AND tab.name = %s
-	AND tab.version = %s) tabtar
+	{{{where_table}}} ) tabtar
 JOIN PARTITION_ par
 ON tabtar.target_id = par.target_id
-ORDER BY tabtar.target_format, par.partition_ts
+{{{where_time}}}
+ORDER BY tabtar.table_name, tabtar.target_format, par.partition_ts;
