4a0550ed5f74b0f8a2aae18ad0eeceb7cf18a84c
Falk Schykowski <f.schykowski@qimia.de>
2020-09-28 15:10:05 +0200

    submit script placeholders changes to cli example

--- uhrwerk-example/src/main/resources/retail_examples/cli_submits/multiple_tables.sh
+++ uhrwerk-example/src/main/resources/retail_examples/cli_submits/multiple_tables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ../../../../../../cli-tools/uhrwerk-start.py \
-dwh.retail_examples.salesFact.1.0 \
+dwh.retail.salesFact.1.0 \
 ../../../../../target/uhrwerk-example-0.1.0-SNAPSHOT.jar \
 --uhrwerk_config ../../testing-env-config.yml \
 --table_configs ../staging/retail/productDimApp_1.0.yml \
@@ -10,4 +10,5 @@ dwh.retail_examples.salesFact.1.0 \
 ../dwh/retail/salesFactApp_1.0.yml \
 --conn_configs ../../testing-connection-config.yml \
 --lower_bound 2020-06-01T00:00:00 \
---upper_bound 2020-06-03T00:00:00
+--upper_bound 2020-06-03T00:00:00 \
+--dag_mode
